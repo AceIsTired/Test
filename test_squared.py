4dc05d7a0383df25857f1f390e0f5a6224dee087
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-10-20 12:42:52 -0500

    Fix indentation in test cases for squared function

--- test_squared.py
+++ test_squared.py
@@ -5,7 +5,7 @@ def test_squared_1:
 	assert squared(2) == 4
 
 def test_squared_2:
-  assert squared(4) == 16
+	assert squared(4) == 16
 
 def test_squared_3:
-  assert squared(8) == 64
+	assert squared(8) == 64
