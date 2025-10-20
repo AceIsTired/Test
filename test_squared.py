3b4c90f249180f08fbdf117c890aa1a8660fbf7d
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-10-20 12:35:42 -0500

    Add squared function and corresponding tests

---
+++ test_squared.py
@@ -0,0 +1,11 @@
+def squared(num):
+  return num * num
+
+def test_squared_1:
+  assert squared(2) == 4
+
+def test_squared_2:
+  assert squared(4) == 16
+
+def test_squared_3:
+  assert squared(8) == 64
