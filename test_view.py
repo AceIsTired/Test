7a5772a47c6a027784d562424e2add566c7cbfa8
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-10-24 12:54:23 -0500

    Refactor test_item to use parameters for testing

--- test_view.py
+++ test_view.py
@@ -1,4 +1,4 @@
+from item import Item
 
-def test_item():
-    item = Item("apple", 4, 10)
-    assert item.str() == "apply $4 10"
+def test_item(name, price, stock):
+    assert Item(name, price, stock) == f"{name}: ${price} | Stock:{stock}"
