d1943bb21821b054044a22c523bf2e75c97abb47
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-10-24 12:59:55 -0500

    Refactor item test cases into separate functions

--- test_view.py
+++ test_view.py
@@ -1,4 +1,10 @@
 from item import Item
 
-def test_item(name, price, stock):
-    assert Item(name, price, stock) == f"{name}: ${price} | Stock:{stock}"
+def test_item1():
+    assert Item("Banana", 1.99, 8) == "Banana: $1.99 | Stock: 8"
+
+def test_item2():
+    assert Item("Apple", 0.99, 10) == "Apple: $0.99 | Stock: 10"
+
+def test_item3():
+    assert Item("Pillow", 11.99, 3) == "Pillow: $11.99 | Stock: 3"
