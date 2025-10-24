622e51f422afbc363ffaf9ffedf94514e9eee6a2
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-10-24 13:02:14 -0500

    Update item tests to use str() method

--- test_view.py
+++ test_view.py
@@ -1,10 +1,10 @@
 from item import Item
 
 def test_item1():
-    assert Item("Banana", 1.99, 8) == "Banana: $1.99 | Stock: 8"
+    assert Item("Banana", 1.99, 8).str() == "Banana: $1.99 | Stock: 8"
 
 def test_item2():
-    assert Item("Apple", 0.99, 10) == "Apple: $0.99 | Stock: 10"
+    assert Item("Apple", 0.99, 10).str() == "Apple: $0.99 | Stock: 10"
 
 def test_item3():
-    assert Item("Pillow", 11.99, 3) == "Pillow: $11.99 | Stock: 3"
+    assert Item("Pillow", 11.99, 3).str() == "Pillow: $11.99 | Stock: 3"
